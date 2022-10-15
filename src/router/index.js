965c410e9fc904d55f9c9e1e42cc381c8148a776
18CTHA0094 <61744125+18CTHA0094@users.noreply.github.com>
2022-10-15 20:16:19 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,20 @@ import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
 const routes = [
 {
-path: "/",
-name: "contactbook",
-component: ContactBook,
+    path: "/",
+    name: "contactbook",
+    component: ContactBook,
 },
+{
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+    },
 ];
+
+
+    
+    
 const router = createRouter({
 history: createWebHistory(import.meta.env.BASE_URL),
 routes,
