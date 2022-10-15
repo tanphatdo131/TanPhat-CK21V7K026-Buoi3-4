080fb0de94a284b76db51e1ae72d2ce1786b70b2
18CTHA0094 <61744125+18CTHA0094@users.noreply.github.com>
2022-10-15 20:10:42 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/App.vue
+++ src/App.vue
@@ -58,3 +58,25 @@ max-width: 400px;
 margin: auto;
 }
 </style>
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+export default {
+components: {
+AppHeader,
+},
+};
+</script>
+<template>
+<div id="app">
+<AppHeader />
+<div class="container mt-3">
+<router-view />
+</div>
+</div>
+</template>
+<style>
+.page {
+max-width: 400px;
+margin: auto;
+}
+</style>
